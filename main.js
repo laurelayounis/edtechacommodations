485a22c4559a7a918d593fd4d8736311b6f3b0f6
Glitch (glitch-hello-node) <none>
2022-05-27 00:50:13 +0000

    🍏🐠 Checkpoint ./server.js:41073631/437:41299569/430:41310042/93:44330987/198:44330998/3075:45256227/1084 ./index.html:41310042/404 ./main.js:41310042/103

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ async function makeReq(){
   const data = await res.json()
 
   console.log(data);
-  document.querySelector("#personName").textContent = data.name
-  document.querySelector("#personStatus").textContent = data.status
-  document.querySelector("#personOccupation").textContent = data.currentOccupation
+  document.querySelector("#name").textContent = data.name
+  document.querySelector("#grade").textContent = data.grade
+  document.querySelector("#accomodations").textContent = data.accomodations
 }
--- server.js
+++ server.js
@@ -35,20 +35,86 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'application/json'});
         const objToJson = {
           name: "leon",
-          accomodations: ["Boss Man"] 
-          currentOccupation: ["Baller", "baller teacher"]
+          grade: 12,
+          accomodations: ["Notetakers and/or audiotaped class sessions", "captioned films", "Extra exam time"]
         }
         res.end(JSON.stringify(objToJson));
       }//student = leon
-      else if(params['student'] != 'leon'){
+      else if(params['student'] == 'luis'){
         res.writeHead(200, {'Content-Type': 'application/json'});
         const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
+          name: "luis",
+          grade: 7,
+          accomodations: ["More food, I get Hangry!", "captioned films", "Extra exam time"]
         }
         res.end(JSON.stringify(objToJson));
       }
+      else if(params['student'] == 'laurel'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "laurel",
+          grade: 10,
+          accomodations: ["Notetaker or copy of another student’s notes",
+"Flexible attendance requirements", "extra exam time",
+"Assignments made available in electronic format"]
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+      else if(params['student'] = 'christina'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "christina",
+          grade: "7",
+          accomodations: ["Flexible attendance", "extra exam time", "A non-distracting, quiet setting for assignments and tests"]
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+      else if(params['student'] == 'eduardo'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "Eduardo",
+          grade: "3",
+          accomodations: ["Extra exam time"]
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+      else if(params['student'] == 'luis'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "luis",
+          grade: "42",
+          accomodations: ["Notetakers and/or audiotaped class sessions", "captioned films", "Extra exam time"]
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+      else if(params['student'] == 'maxwell'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "maxwell",
+          grade: "42",
+          accomodations: ["A non-distracting, quiet setting for assignments and tests", "copy of another student’s notes, or recording of lectures", "Assignments made available in electronic format"]
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+      else if(params['student'] == 'cecil') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "cecil",
+          grade: "42",
+          accomodations: ["Flexible attendance requirements", "captioned films", "Extra exam time"]
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+      else {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "New Student, Please enter student's info.",
+          grade: "unknown",
+          accomodations: []
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+      
     }
   }
   else if (page == '/css/style.css'){
@@ -79,3 +145,17 @@ server.listen(7000);
 
 
 
+//example
+const expr = 'Papayas';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
